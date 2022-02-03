ef27eca0b8d90bc362a4cc3a296e276705d2b613
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-02-03 18:27:41 +0530

    Created Input component
    Created Input component  of different types and with two form
    
    - signup form
    - signIn form
    - input with text
    - input with number
    - input with  password
    - error styling
    - input checkbox
    - input radio button
    - text area input
    - disabled input
    - secondary input

---
+++ docsjs/login.js
@@ -0,0 +1,27 @@
+// const loginForm = document.querySelector("#login-form")
+// const loginName = document.querySelector("#login-name")
+// const loginPassword = document.querySelector("#login-password")
+// const loginCheckbox = document.querySelector("#login-checkbox")
+ 
+//  function error(err){
+
+//  }
+
+// //  function getValue()
+
+// loginForm.addEventListener('submit', (e) =>{
+//     e.preventDefault()
+
+//     let userName = loginName.value 
+//     let userPassword = loginPassword.value 
+//     let check = loginCheckbox.value
+
+//     console.log(check)
+
+//     if(!userName){
+//         error("Enter Your Name")
+//     }else if(!userPassword){
+//         error("Enter Your Password")
+//     }
+     
+// })
