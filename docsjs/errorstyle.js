d58db55e853f36033d8c88474effb84ccc7ded4d
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-02-03 22:25:42 +0530

    Update errorstyle.js

--- docsjs/errorstyle.js
+++ docsjs/errorstyle.js
@@ -2,9 +2,12 @@ const errorInput = document.querySelector("#error-style")
 const errorMessage = document.querySelector("#error-message")
 
 errorInput.addEventListener('input', (e) =>{
-    if(e.target.value.length + 1 > 10){
+
+
+    if(e.target.value.length + 1 > 10 ){
         errorMessage.innerHTML = "Wow you did it"
         errorMessage.style.color = "green"
+
     }else{
         errorMessage.innerHTML = "Enter at least fifteen charecter."
         errorMessage.style.color = "red"
